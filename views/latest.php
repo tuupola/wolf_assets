c6be587d5147fe2537157974b00d2346596bedd8
Mika Tuupola <tuupola@appelsiini.net>
2008-12-18 19:09:13 +0200

    Fix broken <a> tag.

--- views/latest.php
+++ views/latest.php
@@ -1,3 +1,3 @@
 <?php foreach ($image_array as $image => $thumbnail): ?>
-<a href="<?php print $image?>"<img src="<?php print $thumbnail ?>" /></a>
+<a href="<?php print $image?>"><img src="<?php print $thumbnail ?>" /></a>
 <?php endforeach; ?>
