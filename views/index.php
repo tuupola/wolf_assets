7fbc38adc410f70fe5cb6960bceef388a185c652
René Kersten <rene.kersten@gmail.com>
2010-07-30 23:51:30 +0200

    update headline, add translation

--- views/index.php
+++ views/index.php
@@ -1,4 +1,4 @@
-<h1>Assets</h1>
+<h1><?php print __('Assets'); ?></h1>
 
 <form action="<?php print get_url('plugin/assets/upload') ?>" class="new_asset" enctype="multipart/form-data" method="post">
 <input name="user_file" type="file" />
